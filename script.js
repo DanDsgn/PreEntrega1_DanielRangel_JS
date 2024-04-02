7e228ca0cd3bdab6d1330e899d7c7240b05046c9
Daniel Rangel <danbar.dg@gmail.com>
2024-04-02 01:28:40 -0600

    Actualización

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let edad = Number ( prompt ("Ingresa tu edad"))
 let nombre = prompt ( "ingresa tu nombre")
 
 if (edad > 30) {
-    console.log(nombre + " ya eres viejo");
+    alert (nombre + " ya eres viejo");
 } else if (edad <= 30 && edad > 20) {
-    console.log(nombre + " te estas haciendo viejo");
+    alert (nombre + " te estas haciendo viejo");
 } else if (edad <= 20){
-    console.log(nombre + " estas muy joven");
+    alert (nombre + " estas muy joven");
 }
